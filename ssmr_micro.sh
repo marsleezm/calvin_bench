d4704067a4c4d87182c505514b9905a569fe7364
Zhongmiao Li <zholi@granduc-1.luxembourg.grid5000.fr>
2017-07-15 02:23:42 +0200

    Add change

--- ssmr_micro.sh
+++ ssmr_micro.sh
@@ -2,64 +2,64 @@
 
 # Folder, bench type, m_batch, dist_p, dep_p, index_num, index_size, max_sc, max_pend, max_suspend
 seq="1 2"
-cs="1000 50000"
+cs="1000"
 #deps="0 50 100"
-deps="100 50 0"
+deps="0"
 for s in $seq
 do
 	for c in $cs 
 	do
 		for dep in $deps
 		do
-				./benchmark.sh calvin_less_recon m 100 1 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 200 1 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 300 1 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 400 1 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 500 1 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 100 1 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 200 1 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 300 1 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 400 1 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 500 1 $dep 5 $c 10 1 10 2 200 false
 
-				./benchmark.sh calvin_less_recon m 100 50 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 200 50 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 250 50 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 300 50 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 400 50 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 100 50 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 200 50 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 250 50 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 300 50 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 400 50 $dep 5 $c 10 1 10 2 200 false
 	
-				./benchmark.sh calvin_less_recon m 100 0 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 200 0 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 400 0 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 600 0 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 800 0 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 100 0 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 200 0 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 400 0 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 600 0 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 800 0 $dep 5 $c 10 1 10 2 200 false
 
-				#./benchmark.sh calvin_less_recon m 100 0.01 $dep 5 $c 10 1 10 2 200 false
-				#./benchmark.sh calvin_less_recon m 200 0.01 $dep 5 $c 10 1 10 2 200 false
-				#./benchmark.sh calvin_less_recon m 400 0.01 $dep 5 $c 10 1 10 2 200 false
-				#./benchmark.sh calvin_less_recon m 600 0.01 $dep 5 $c 10 1 10 2 200 false
-				#./benchmark.sh calvin_less_recon m 800 0.01 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 100 0.01 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 200 0.01 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 400 0.01 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 600 0.01 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 800 0.01 $dep 5 $c 10 1 10 2 200 false
 
-				#./benchmark.sh calvin_less_recon m 100 0.1 $dep 5 $c 10 1 10 2 200 false
-				#./benchmark.sh calvin_less_recon m 200 0.1 $dep 5 $c 10 1 10 2 200 false
-				#./benchmark.sh calvin_less_recon m 400 0.1 $dep 5 $c 10 1 10 2 200 false
-				#./benchmark.sh calvin_less_recon m 550 0.1 $dep 5 $c 10 1 10 2 200 false
-				#./benchmark.sh calvin_less_recon m 700 0.1 $dep 5 $c 10 1 10 2 200 false
-
-
-				./benchmark.sh calvin_less_recon m 100 10 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 200 10 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 400 10 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 500 10 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 600 10 $dep 5 $c 10 1 10 2 200 false
-
-				#./benchmark.sh calvin_less_recon m 100 20 $dep 5 $c 10 1 10 2 200 false
-				#./benchmark.sh calvin_less_recon m 200 20 $dep 5 $c 10 1 10 2 200 false
-				#./benchmark.sh calvin_less_recon m 350 20 $dep 5 $c 10 1 10 2 200 false
-				#./benchmark.sh calvin_less_recon m 450 20 $dep 5 $c 10 1 10 2 200 false
-				#./benchmark.sh calvin_less_recon m 550 20 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 100 0.1 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 200 0.1 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 400 0.1 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 550 0.1 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 700 0.1 $dep 5 $c 10 1 10 2 200 false
 
 
-				./benchmark.sh calvin_less_recon m 100 100 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 150 100 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 200 100 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 250 100 $dep 5 $c 10 1 10 2 200 false
-				./benchmark.sh calvin_less_recon m 300 100 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 100 10 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 200 10 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 400 10 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 500 10 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 600 10 $dep 5 $c 10 1 10 2 200 false
+
+				#./benchmark.sh ssmr m 100 20 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 200 20 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 350 20 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 450 20 $dep 5 $c 10 1 10 2 200 false
+				#./benchmark.sh ssmr m 550 20 $dep 5 $c 10 1 10 2 200 false
+
+
+				./benchmark.sh ssmr m 100 100 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 150 100 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 200 100 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 250 100 $dep 5 $c 10 1 10 2 200 false
+				./benchmark.sh ssmr m 300 100 $dep 5 $c 10 1 10 2 200 false
 		done
 	done
 done
